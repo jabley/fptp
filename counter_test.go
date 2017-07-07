6c9712408414cbdd1937f9a8cd0a0bf7607486fc
James Abley <james.abley@gmail.com>
2017-07-07 13:30:26 +0100

    Implement safe fan-out and return quickest response
    This is a correct implementation that will:
    
    - fan-out a request to multiple backing services
    - return the first non-error result
    - ensure slow responses are all cleaned up correctly so no resources are
      leaked

---
+++ counter_test.go
@@ -0,0 +1,62 @@
+package fptp_test
+
+import (
+	"io"
+	"sync"
+
+	"github.com/jabley/fptp"
+)
+
+type closeObserver interface {
+	Closed()
+}
+
+type closeDispenser interface {
+	NewCloser() io.Closer
+}
+
+type CounterCloser struct {
+	mu            sync.Mutex
+	awaitingClose int
+}
+
+func (c *CounterCloser) NewCloser() io.Closer {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.awaitingClose++
+	return &countingCloser{closeObserver: c}
+}
+
+func (c *CounterCloser) Closed() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.awaitingClose--
+}
+
+func (c *CounterCloser) Unclosed() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.awaitingClose
+}
+
+type countingCloser struct {
+	closeObserver closeObserver
+}
+
+func (cc *countingCloser) Close() error {
+	cc.closeObserver.Closed()
+	return nil
+}
+
+// NewCountingSearcher returns a Searcher that uses the provided CounterCloser to generate responses.
+func NewCountingSearcher(dispenser *CounterCloser) fptp.Searcher {
+	return &basicSearcher{dispenser: dispenser}
+}
+
+type basicSearcher struct {
+	dispenser *CounterCloser
+}
+
+func (bs *basicSearcher) Search(req *fptp.SearchRequest) (io.Closer, error) {
+	return bs.dispenser.NewCloser(), nil
+}
